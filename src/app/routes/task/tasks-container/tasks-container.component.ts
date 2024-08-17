3145403c096045382b1728fcb601b3a07b316b48
ehvidalp <ehvidalp@gmail.com>
2024-08-17 11:56:17 -0600

    Add task container header

--- src/app/routes/task/tasks-container/tasks-container.component.ts
+++ src/app/routes/task/tasks-container/tasks-container.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SquareButtonComponent } from '@shared/ui/square-button/square-button.component';
+import { SquareFieldComponent } from '@shared/ui/square-field/square-field.component';
 import { SquareHeaderComponent } from '@shared/ui/square-header/square-header.component';
 import { SquareTaskComponent } from '@shared/ui/square-task/square-task.component';
 
@@ -7,7 +9,7 @@ import { SquareTaskComponent } from '@shared/ui/square-task/square-task.componen
   selector: 'app-tasks-container',
   standalone: true,
   imports: [
-    CommonModule, SquareHeaderComponent, SquareTaskComponent
+    CommonModule, SquareHeaderComponent, SquareTaskComponent, SquareButtonComponent, SquareFieldComponent
   ],
   templateUrl: './tasks-container.component.html',
   styleUrl: './tasks-container.component.scss',
